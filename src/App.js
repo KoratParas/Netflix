223cc3ebdb661de01879cfc2dedb4bc333f82896
ParasK <koratparas@gmail.com>
2024-11-25 21:43:46 +1100

    Done: firebase login/signup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginScreen from "./screens/LoginScreen";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (userAuth) => {
+      if (userAuth) {
+        //Logged in
+      } else {
+        //Logged out
+      }
+    });
+
+    return unsubscribe;
+  }, []);
   return (
     <div className="app">
       <Router>
